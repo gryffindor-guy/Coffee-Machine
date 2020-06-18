d9d78ea64759220208a9c0f3aa532ad2a0c90759
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2020-06-18 09:53:21 +0530

    message "stage 2 completed"

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,9 @@
 package machine;
+import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Starting to make a coffee\n" +
                 "Grinding coffee beans\n" +
                 "Boiling water\n" +
@@ -9,5 +11,11 @@ public class CoffeeMachine {
                 "Pouring coffee into the cup\n" +
                 "Pouring some milk into the cup\n" +
                 "Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+        System.out.println("For " + cups + " cups of coffee you will need:");
+        System.out.println(cups * 200 + " ml of water");
+        System.out.println(cups * 50 + " ml of milk");
+        System.out.println(cups * 15 + " g of coffee beans");
     }
 }
