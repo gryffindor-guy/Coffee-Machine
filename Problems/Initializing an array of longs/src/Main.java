e85c0186d0d371c128f923d0799ffcbfec41e1ac
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2020-06-18 16:59:09 +0530

    problem solved "Initializing an array of longs "

---
+++ Problems/Initializing an array of longs/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long[] longNumbers = {100000000001L, 100000000002L, 100000000003L};
+
+        System.out.println(Arrays.toString(longNumbers));
+    }
+}
