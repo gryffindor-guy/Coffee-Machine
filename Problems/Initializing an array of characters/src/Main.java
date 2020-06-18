83c7644333c31b3676e5ba9d4587fce8a98b7abd
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2020-06-18 16:46:11 +0530

    coffee machine "Initializing an array of characters "

---
+++ Problems/Initializing an array of characters/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char[] characters;
+        characters = new char[] {'a','z','e','d'};
+
+        System.out.println(Arrays.toString(characters));
+    }
+}
