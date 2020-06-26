5f980193f3f14e2b34b7c39fc499321da819412f
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2020-06-26 13:48:25 +0530

    problem completed "The longest ascending sequence"

--- Problems/The longest ascending sequence/src/Main.java
+++ Problems/The longest ascending sequence/src/Main.java
@@ -10,7 +10,7 @@ class Main {
             arr[i] = scanner.nextInt();
         }
         for (int i = 0; i < length - 1; i++) {
-            if (arr[i] < arr[i+1]) {
+            if (arr[i] < arr[i + 1]) {
                 tempCount++;
             } else {
                 tempCount = 1;
